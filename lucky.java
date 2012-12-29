902f3dfe12565dc21aa809b0b8bc654087c1e959
Rohan Dalvi <me@rohandalvi.com>
2012-12-29 03:15:43 -0800

    Second commit
    Compliant with standard input and revised changes as made to readme.txt

--- lucky.java
+++ lucky.java
@@ -4,30 +4,48 @@
     Version: 1.0
  */
 /*
- More changes to be made, changing command line input to stdin, using bufferedreader or scanner.
+ Accepting Standard Input, compliant with the test cases as mentioned in the revised readme.
  */
-public class lucky {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		long[] numbers = new long[1000*1000];
+import java.util.Scanner;
+public class Solution {
+	public static void main(String args[])
+	{
+        long[] numbers = new long[1000*1000];
 		char[] no = new char[10000000];
 		int count=0;
-		for(int i=Integer.parseInt(args[0]);i<=Integer.parseInt(args[1]);i++)
+		Scanner sc = new Scanner(System.in);
+		int i=0;
+        String[] s = new String[5000];
+		while(sc.hasNextLine())
+		{
+			s[i]=sc.next();
+            i++;
+        }
+		for(int r=Integer.parseInt(s[1]);r<=Integer.parseInt(s[2]);r++)
 		{
 			
-			if(islucky(i)){
+			if(islucky(r))
+            {
 				count++;
-			}
-
+            }
+            
 		}
 		System.out.println(count);
-
+        count=0;
+        for(int r=Integer.parseInt(s[3]);r<=Integer.parseInt(s[4]);r++)
+		{
+			
+			if(islucky(r))
+            {
+				count++;
+            }
+            
+		}
+        System.out.println(count);
+        
+		
 	}
-	public static boolean islucky(int nos)
+    public static boolean islucky(int nos)
 	{
 		long sum=0;
 		long square_sum=0;
@@ -40,10 +58,10 @@ public class lucky {
 		}
 		if(nos/10==0)
 			sum+=nos%10;
-			square_sum+=(nos%10)*(nos%10);
-
+        square_sum+=(nos%10)*(nos%10);
+        
 		if(isPrime(sum) && isPrime(square_sum))
-		return true;
+            return true;
 		else
 			return false;
 	}
@@ -60,5 +78,5 @@ public class lucky {
 		else
 			return false;
 	}
-
+    
 }
